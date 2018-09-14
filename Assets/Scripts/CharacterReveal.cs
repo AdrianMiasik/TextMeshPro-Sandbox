6217c91c030deda1c293a91900505b9503fc66b0
Adrian Miasik <AdrianMiasik@gmail.com>
2018-09-14 01:33:01 -0400

    Modified `CharacterReveal.cs` - Added a replace function that will replace the string inside _text

--- Assets/Scripts/CharacterReveal.cs
+++ Assets/Scripts/CharacterReveal.cs
@@ -6,116 +6,132 @@
 // Purpose: Reveals text characters over time much like a typewriter (Using the TextMeshProUGUI component). There is a delay in-between each character reveal. 
 
 using UnityEngine;
+using TMPro;
 
-public class CharacterReveal : MonoBehaviour
+namespace AdrianMiasik.Text
 {
-	[SerializeField] 
-	[Tooltip("The text we want to reveal.")] 
-	private TMPro.TextMeshProUGUI _text;
-
-	[SerializeField] 
-	[Tooltip("The text we will use to display statistics about this character reveal (Number of characters revealed, the delay in-between each character reveal, and the current characters reveal time).")]
-	private TMPro.TextMeshProUGUI _statistics;
-
-	[SerializeField] 
-	[Tooltip("The keycode that starts the reveal. When pressing this key down, the reveal will start.")] 
-	private KeyCode _startKey = KeyCode.Space;
-
-	[SerializeField] 
-	[Tooltip("The amount of time (in seconds) between each character reveal.")]
-	private float _delayBetweenReveal = 0.05f;
-
-	private bool _isRevealing = false;
-	private float _characterTime = 0f;
-	private int _numberOfCharacters = 0;
-	private int _numberOfCharactersRevealed;
-
-	private void Reset()
+	public class CharacterReveal : MonoBehaviour
 	{
-		_text = GetComponent<TMPro.TextMeshProUGUI>();
-		_statistics = GetComponent<TMPro.TextMeshProUGUI>();
-	}
+		[SerializeField] [Tooltip("The text we want to reveal.")]
+		private TextMeshProUGUI _text;
 
-	private void Awake()
-	{
-		if (!_text)
+		[SerializeField] [Tooltip("The text we will use to display statistics about this character reveal (Number of characters revealed, the delay in-between each character reveal, and the current characters reveal time).")]
+		private TextMeshProUGUI _statistics;
+
+		[SerializeField] [Tooltip("The keycode that starts the reveal. When pressing this key down, the reveal will start.")]
+		private KeyCode _startKey = KeyCode.Space;
+
+		[SerializeField] [Tooltip("The amount of time (in seconds) between each character reveal.")]
+		private float _delayBetweenReveal = 0.05f;
+
+		private bool _isRevealing = false;
+		private float _characterTime = 0f;
+		private int _numberOfCharacters = 0;
+		private int _numberOfCharactersRevealed;
+
+		private void Reset()
 		{
-			_text = GetComponent<TMPro.TextMeshProUGUI>();
-		}
-	}
-
-	private void Start()
-	{
-		if (!_text)
-		{
-			Debug.LogWarning("No TextMeshProUGUI component found.");
+			_text = GetComponent<TextMeshProUGUI>();
+			_statistics = GetComponent<TextMeshProUGUI>();
 		}
 
-		if (!_statistics)
+		private void Awake()
 		{
-			Debug.LogWarning("No TextMeshProUGUI component found.");
-		}
-		else
-		{
-			InitializeTextReveal();
-			UpdateStatisticsText();
-		}
-	}
-
-	/// <summary>
-	/// Initializes the text reveal by hiding the text and defaulting some variables
-	/// </summary>
-	private void InitializeTextReveal()
-	{
-		_text.maxVisibleCharacters = 0;
-		_numberOfCharactersRevealed = 0;
-		_numberOfCharacters = _text.textInfo.characterCount;
-	}
-
-	/// <summary>
-	/// Updates the statistics UI string
-	/// </summary>
-	private void UpdateStatisticsText()
-	{
-		_statistics.text = "Number of characters revealed: " + _numberOfCharactersRevealed + "/" + _numberOfCharacters + "\nDelay in-between character reveal: " + _delayBetweenReveal + " seconds.\nCurrent character reveal time: " + _characterTime.ToString("F2") + " seconds.";
-	}
-
-	private void Update()
-	{
-		// Prevent a negative value from happening
-		_delayBetweenReveal = Mathf.Clamp(_delayBetweenReveal, 0, _delayBetweenReveal);
-
-		// Start text reveal on input
-		if (Input.GetKeyDown((_startKey)))
-		{
-			InitializeTextReveal();
-			_isRevealing = true;
-		}
-
-		// If we are text revealing...
-		if (_isRevealing)
-		{
-			_characterTime += Time.deltaTime;
-
-			// While loop used to calculate how many letters on the same frame needs to be drawn
-			while (_characterTime > _delayBetweenReveal)
+			if (!_text)
 			{
-				// Reveal a character
-				_text.maxVisibleCharacters = _numberOfCharactersRevealed + 1 % _numberOfCharacters;
+				_text = GetComponent<TextMeshProUGUI>();
+			}
+		}
 
-				_numberOfCharactersRevealed++;
-				_characterTime -= _delayBetweenReveal;
-
-				// If all characters are revealed, set the _isRevealing flag as dirty and break out of this while loop 
-				if (_numberOfCharactersRevealed == _numberOfCharacters)
-				{
-					_characterTime = 0f;
-					_isRevealing = false;
-					break;
-				}
+		private void Start()
+		{
+			if (!_text)
+			{
+				Debug.LogWarning("No TextMeshProUGUI component found.");
 			}
 
-			UpdateStatisticsText();
+			if (!_statistics)
+			{
+				Debug.LogWarning("No TextMeshProUGUI component found.");
+			}
+			else
+			{
+				InitializeTextReveal();
+				_isRevealing = true;
+			}
+		}
+
+		/// <summary>
+		/// Initializes the text reveal by hiding the text and defaulting some variables
+		/// </summary>
+		private void InitializeTextReveal()
+		{
+			_text.maxVisibleCharacters = 0;
+			_numberOfCharactersRevealed = 0;
+			_numberOfCharacters = _text.textInfo.characterCount;
+		}
+
+		/// <summary>
+		/// Updates the statistics UI string
+		/// </summary>
+		private void UpdateStatisticsText()
+		{
+			_statistics.text = "Number of characters revealed: " + _numberOfCharactersRevealed + "/" + _numberOfCharacters + "\nDelay in-between character reveal: " + _delayBetweenReveal + " seconds.\nCurrent character reveal time: " + _characterTime.ToString("F2") + " seconds.";
+		}
+
+		/// <summary>
+		/// Replaces the text string in _text
+		/// </summary>
+		/// <param name="text"></param>
+		public void Replace(TextMeshProUGUI source)
+		{
+			// Stop reveal
+			_isRevealing = false;
+			
+			// Replace text
+			_text.text = source.text;
+			
+			// Reveals all the characters
+			_text.maxVisibleCharacters = source.textInfo.characterCount;
+		}
+
+		private void Update()
+		{
+			// Prevent a negative value from happening
+			_delayBetweenReveal = Mathf.Clamp(_delayBetweenReveal, 0, _delayBetweenReveal);
+
+			// Start text reveal on input
+			if (Input.GetKeyDown((_startKey)))
+			{
+				InitializeTextReveal();
+				_isRevealing = true;
+			}
+
+			// If we are text revealing...
+			if (_isRevealing)
+			{
+				_characterTime += Time.deltaTime;
+
+				// While loop used to calculate how many letters on the same frame needs to be drawn
+				while (_characterTime > _delayBetweenReveal)
+				{
+					// Reveal a character
+					_text.maxVisibleCharacters = _numberOfCharactersRevealed + 1 % _numberOfCharacters;
+
+					_numberOfCharactersRevealed++;
+					_characterTime -= _delayBetweenReveal;
+
+					// If all characters are revealed, set the _isRevealing flag as dirty and break out of this while loop 
+					if (_numberOfCharactersRevealed == _numberOfCharacters)
+					{
+						_characterTime = 0f;
+						_isRevealing = false;
+						break;
+					}
+				}
+
+				UpdateStatisticsText();
+			}
 		}
 	}
 }
