c630abd40d2026fa65e5974995969b2c315081f8
Adrian Miasik <AdrianMiasik@gmail.com>
2019-01-31 23:43:23 -0500

    Cleaned up functions within `TypeWriter.cs` so it can be accessed easily. Also provided and created an example which can be seen inside `TypeWriterUsageExample.cs`.

--- Assets/Scripts/Text/Reveal/TypeWriter.cs
+++ Assets/Scripts/Text/Reveal/TypeWriter.cs
@@ -22,21 +22,18 @@ namespace Text.Reveal
 		[SerializeField] [Tooltip("The text we will use to display statistics about this character reveal (Number of characters revealed, the delay in-between each character reveal, and the current characters reveal time).")]
 		private TextMeshProUGUI statistics;
 
-		[SerializeField] [Tooltip("The keycode that starts the reveal. When pressing this key down, the reveal will start.")]
-		private KeyCode startKey = KeyCode.Space;
-
 		[SerializeField] [Tooltip("The amount of time (in seconds) between each character reveal.")]
-		private float delayBetweenReveal = 0.05f;
+		private float characterDelay = 0.05f;
 
-		private bool _isRevealing = false;
-		private float _characterTime = 0f;
+		private bool _isRevealing;
+		private float _characterTime;
 		private float _totalRevealTime;
 		private int _numberOfCharacters = 0;
 		private int _numberOfCharactersRevealed;
 		
 		private void Reset()
 		{
-			Profiler.BeginSample("TypeWriter.Reset");
+			Profiler.BeginSample("TypeWriter.Reset()");
 			
 			// Quickly Fetch References.
 			displayText = GetComponent<TextMeshProUGUI>();
@@ -47,7 +44,7 @@ namespace Text.Reveal
 
 		private void Awake()
 		{
-			Profiler.BeginSample("TypeWriter.Awake");
+			Profiler.BeginSample("TypeWriter.Awake()");
 
 			// If displayText is null...
 			if (displayText == null)
@@ -69,7 +66,7 @@ namespace Text.Reveal
 
 		private void Start()
 		{
-			Profiler.BeginSample("TypeWriter.Start");
+			Profiler.BeginSample("TypeWriter.Start()");
 
 			if (!displayText)
 			{
@@ -90,11 +87,11 @@ namespace Text.Reveal
 		}
 
 		/// <summary>
-		/// Initializes the text (which also means hiding it) and starts the character reveal within the update loop.
+		/// Initialize the text, and starts the character reveal.
 		/// </summary>
 		public void Reveal()
 		{
-			Profiler.BeginSample("TypeWriter.Reveal");
+			Profiler.BeginSample("TypeWriter.Reveal()");
 			
 			Initialize();
 			Play();
@@ -103,11 +100,88 @@ namespace Text.Reveal
 		}
 
 		/// <summary>
+		/// Set the text string, initialize the text, and start the character reveal.
+		/// </summary>
+		/// <param name="message"></param>
+		public void Reveal(string message)
+		{
+			Profiler.BeginSample("TypeWriter.Reveal(string)");
+			
+			SetTextString(message);
+			Initialize();
+			Play();
+
+			Profiler.EndSample();
+		}
+
+		/// <summary>
+		/// Set the time between each character reveal, set the text string, initialize the text, and start the character reveal.
+		/// </summary>
+		/// <param name="message"></param>
+		/// <param name="timeBetweenCharacterReveal"></param>
+		public void Reveal(float timeBetweenCharacterReveal, string message)
+		{
+			Profiler.BeginSample("TypeWriter.Reveal(float, string)");
+
+			SetCharacterDelay(timeBetweenCharacterReveal);
+			Reveal(message);
+			
+			Profiler.EndSample();
+		}
+		
+		// TMP INPUT FIELD - Incorrect character length value of the input fields text component:
+		// The reason we are doing TMP_InputField instead of getting the TextMeshProUGUI component within the input field component
+		// is because the TextMeshProUGUI text.Length and textInfo.CharacterCount values are incorrect. The text field could be an "empty"
+		// string, but it still seems to provides us with a length of 1. 
+		// According to a Unity Technologies user "Stephan_B" we shouldn't be accessing the input fields text component anyways.
+		// However, if you do need to access the text input's TextMeshProUGUI component and you do need to get the character count value from
+		// that component, there does seem to be a workaround a Unity forum user named "Chris-Trueman" has found.
+		// Simply trimming the Unicode character 'ZERO WIDTH SPACE' (Code 8203) from the ugui text string does seem to return the correct length.
+		// I've tested Chris' solution and it does seem to work if you need to use it, however for this case I won't be doing that.
+		// SOURCE: https://forum.unity.com/threads/textmesh-pro-ugui-hidden-characters.505493/
+		/// <summary>
+		/// Replaces the text string with the provided source's text string (Using the TextMeshProUGUI component).
+		/// </summary>
+		/// <param name="source"></param>
+		public void ReplaceStringWithSources(TMP_InputField source)
+		{
+			Profiler.BeginSample("TypeWriter.ReplaceStringWithSources(TMP_InputField)");
+			displayText.text = source.text;
+			Profiler.EndSample();
+		}
+
+		/// <summary>
+		/// Set the amount of time between each character reveal.
+		/// </summary>
+		/// <param name="timeInSeconds"></param>
+		private void SetCharacterDelay(float timeInSeconds)
+		{
+			Profiler.BeginSample("TypeWriter.SetCharacterDelay(float)");
+			
+			characterDelay = timeInSeconds;
+			
+			Profiler.EndSample();
+		}
+
+		/// <summary>
+		/// Set the text string.
+		/// </summary>
+		/// <param name="message"></param>
+		private void SetTextString(string message)
+		{
+			Profiler.BeginSample("TypeWriter.SetTextString(string)");
+			
+			displayText.text = message;
+			
+			Profiler.EndSample();
+		}
+		
+		/// <summary>
 		/// Initializes the text reveal by hiding the text and defaulting some variables.
 		/// </summary>
 		private void Initialize()
 		{
-			Profiler.BeginSample("TypeWriter.Initialize");
+			Profiler.BeginSample("TypeWriter.Initialize()");
 
 			// Hide the text
 			displayText.maxVisibleCharacters = 0;
@@ -125,10 +199,10 @@ namespace Text.Reveal
 		/// </summary>
 		private void UpdateStatisticsText()
 		{
-			Profiler.BeginSample("TypeWriter.UpdateStatisticsText");
+			Profiler.BeginSample("TypeWriter.UpdateStatisticsText()");
 
 			statistics.text = "Number of characters revealed: " + _numberOfCharactersRevealed + "/" + _numberOfCharacters + "\n" +
-			                  "Delay in-between character reveal: " + delayBetweenReveal + " seconds.\n" +
+			                  "Delay in-between character reveal: " + characterDelay + " seconds.\n" +
 			                  "Current character reveal time: " + _characterTime.ToString("F2") + " seconds.\n" +
 			                  "Elapsed time: " + _totalRevealTime.ToString("F2") + " seconds.\n";
 			
@@ -139,50 +213,24 @@ namespace Text.Reveal
 		/// Starts revealing the characters.
 		/// </summary>
 		/// <summary>
-		/// Note: You might need to initialize this script first.
+		/// Note: You might need to call Initialize() first.
 		/// </summary>
 		private void Play()
 		{
-			Profiler.BeginSample("TypeWriter.Play");
+			Profiler.BeginSample("TypeWriter.Play()");
 			
 			_isRevealing = true;
 			
 			Profiler.EndSample();
 		}
 
-		// TMP INPUT FIELD - Incorrect character length value of the input fields text component:
-		// The reason we are doing TMP_InputField instead of getting the TextMeshProUGUI component within the input field component
-		// is because the TextMeshProUGUI text.Length and textInfo.CharacterCount values are incorrect. The text field could be an "empty"
-		// string, but it still seems to provides us with a length of 1. 
-		// According to a Unity Technologies user "Stephan_B" we shouldn't be accessing the input fields text component anyways.
-		// However, if you do need to access the text input's TextMeshProUGUI component and you do need to get the character count value from
-		// that component, there does seem to be a workaround a Unity forum user named "Chris-Trueman" has found.
-		// Simply trimming the Unicode character 'ZERO WIDTH SPACE' (Code 8203) from the ugui text string does seem to return the correct length.
-		// I've tested Chris' solution and it does seem to work if you need to use it, however for this case I won't be doing that.
-		// SOURCE: https://forum.unity.com/threads/textmesh-pro-ugui-hidden-characters.505493/
-		/// <summary>
-		/// Caches the string in the source's text string. (Using the TextMeshProUGUI component).
-		/// </summary>
-		/// <param name="source"></param>
-		public void SetText(TMP_InputField source)
-		{
-			Profiler.BeginSample("TypeWriter.SetText");
-			displayText.text = source.text;
-			Profiler.EndSample();
-		}
 
 		private void Update()
 		{
-			Profiler.BeginSample("TypeWriter.Update");
+			Profiler.BeginSample("TypeWriter.Update()");
 
 			// Prevent a negative value from being assigned
-			delayBetweenReveal = Mathf.Clamp(delayBetweenReveal, 0, delayBetweenReveal);
-
-			// Start text reveal on input
-			if (Input.GetKeyDown(startKey))
-			{
-				Reveal();
-			}
+			characterDelay = Mathf.Clamp(characterDelay, 0, characterDelay);
 
 			// If we are text revealing...
 			if (_isRevealing)
@@ -202,13 +250,13 @@ namespace Text.Reveal
 				_characterTime += Time.deltaTime;
 
 				// While loop used to calculate how many letters on the same frame needs to be drawn
-				while (_characterTime > delayBetweenReveal)
+				while (_characterTime > characterDelay)
 				{				
 					// Reveal a character
 					_numberOfCharactersRevealed++;
 					displayText.maxVisibleCharacters = _numberOfCharactersRevealed;
 
-					_characterTime -= delayBetweenReveal;
+					_characterTime -= characterDelay;
 					
 					// If all characters are revealed, set the _isRevealing flag as dirty and break out of this while loop 
 					if (_numberOfCharactersRevealed == _numberOfCharacters)
