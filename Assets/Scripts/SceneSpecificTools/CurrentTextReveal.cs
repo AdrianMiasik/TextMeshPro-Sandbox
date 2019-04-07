998db4c3b8244cd9a78f8d49d7c850c0ab88e4e9
Adrian Miasik <AdrianMiasik@gmail.com>
2019-04-07 03:04:40 -0400

    Created `CurrentTextReveal.cs` so I can quickly switch between the different text reveals without having to swap/change 4 references inside the `Text Character Reveal.unity` scene everytime.

---
+++ Assets/Scripts/SceneSpecificTools/CurrentTextReveal.cs
@@ -0,0 +1,23 @@
+﻿using Text.Reveals.Base;
+using TMPro;
+using UnityEngine;
+
+namespace SceneSpecificTools
+{
+	// A script used to quickly change between the text reveals without having to always change the references in the scene.
+	// You can ignore this script.
+	public class CurrentTextReveal : MonoBehaviour
+	{
+		public TextReveal TextReveal;
+
+		public void ReplaceStringWithSources(TMP_InputField source)
+		{
+			TextReveal.ReplaceStringWithSources(source);
+		}
+
+		public void Reveal()
+		{
+			TextReveal.Reveal();
+		}
+	}
+}
